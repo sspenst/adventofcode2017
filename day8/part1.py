89acf17bdfe86f9065001f349c9f168d44051fc2
sspenst <spencerspenst@gmail.com>
2017-12-10 01:38:51 -0800

    day 8 solutions

---
+++ day8/part1.py
@@ -0,0 +1,42 @@
+def check_cond(x, cond, y):
+	return {
+		'<': lambda x, y: x < y,
+		'>': lambda x, y: x > y,
+		'==': lambda x, y: x == y,
+		'!=': lambda x, y: x != y,
+		'<=': lambda x, y: x <= y,
+		'>=': lambda x, y: x >= y
+	}[cond](x, y)
+
+with open('input', 'r') as f:
+	lines = f.read().strip().split("\n")
+	regs = {}
+
+	for line in lines:
+		inst = line.split(" ")
+		rd = inst[0]
+		op = inst[1]
+		amt = int(inst[2])
+		rc = inst[4]
+		cond = inst[5]
+		val = int(inst[6])
+		
+		if rc not in regs:
+			regs[rc] = 0
+
+		if check_cond(regs[rc], cond, val):
+			if rd not in regs:
+				regs[rd] = 0
+
+			if op == 'inc':
+				regs[rd] += amt
+			elif op == 'dec':
+				regs[rd] -= amt
+
+	max = None
+
+	for val in regs.values():
+		if max == None or val > max:
+			max = val
+
+	print(max)
